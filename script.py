07e9f03fbadaf78794552e6cbb123a649d3bd410
sponkurtus2 <sponkurtus3@gmai.com>
2023-10-10 16:08:31 -0600

    Telegram bot using python

---
+++ script.py
@@ -0,0 +1,18 @@
+import telebot
+
+# BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = '6504422559:AAExHFJfH5iv9_PXrI4HnD3b3n_oljSu2zk'
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Hey Gay, how are you doing???")
+
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
